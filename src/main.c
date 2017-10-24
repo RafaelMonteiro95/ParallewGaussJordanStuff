31c0a0a494e71ff92e2ae896131a75534d419f3f
RafaelMonteiro95 <rafaelmonteiro95@gmail.com>
2017-10-24 14:28:57 +0000

    Created main

---
+++ src/main.c
@@ -0,0 +1,10 @@
+/*Authors:
+	Cho - 1234567789
+	Giovanna Oliveira Guimarães - 9278693
+	Lucas Alexandre Soares - 9293265
+	Rafael Augusto Monteiro - 9293095*/
+
+int main(int argc, char* argv[]) {
+
+	return 0;
+}
